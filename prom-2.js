861ba0356ef24d3e09172ed3336ea4ec8b30f753
Alejo Boga <alejoboga20@gmail.com>
2023-05-04 14:50:34 -0400

    catch error

--- prom-2.js
+++ prom-2.js
@@ -1,5 +1,7 @@
 const addOne = (number) => {
 	let promise = new Promise((resolve, reject) => {
+		if (number >= 12) reject(`Number is: ${number}`);
+
 		setTimeout(() => resolve(number + 1), 800);
 	});
 
@@ -9,4 +11,5 @@ const addOne = (number) => {
 addOne(10)
 	.then(addOne)
 	.then(addOne)
-	.then((result) => console.log({ result }));
+	.then((result) => console.log({ result }))
+	.catch((error) => console.log({ error }));
