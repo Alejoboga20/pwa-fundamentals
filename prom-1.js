3e28c7eaa1121969e49eb901436b7b33d7025062
Alejo Boga <alejoboga20@gmail.com>
2023-05-04 11:34:14 -0400

    problem with callbacks

---
+++ prom-1.js
@@ -0,0 +1,23 @@
+/* Callback */
+const addOne = (number, callback) => {
+	setTimeout(() => {
+		callback(number + 1);
+	}, 800);
+};
+
+addOne(10, (result) => console.log({ result }));
+
+/* Concat callbacks */
+const restOne = (number, callback) => {
+	if (number <= 1) return callback('invalid number');
+
+	setTimeout(() => {
+		callback(number - 1);
+	}, 800);
+};
+
+restOne(2, (result) => {
+	restOne(result, (result) => {
+		restOne(result, (result) => console.log({ result }));
+	});
+});
