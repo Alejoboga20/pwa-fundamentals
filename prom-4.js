503d0e2f63dc2980731a81ac218a3bf7a0d7f84c
Alejo Boga <alejoboga20@gmail.com>
2023-05-05 11:07:05 -0400

    promise race

---
+++ prom-4.js
@@ -0,0 +1,13 @@
+const addSlowlly = (number) =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => resolve(number + 1), 800);
+	});
+
+const addFast = (number) =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => resolve(number + 1), 300);
+	});
+
+Promise.all([addSlowlly(10), addFast(15)]).then((result) => console.log({ result }));
+
+Promise.race([addSlowlly(10), addFast(15)]).then((result) => console.log({ result }));
